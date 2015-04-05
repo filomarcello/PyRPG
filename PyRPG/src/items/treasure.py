fd07b6ee29cea7267dbcf896d27e651185527b15
filomarcello <marcello.filopanti@gmail.com>
2015-04-05 22:59:26 +0200

    Add treasure and weapon modules

---
+++ PyRPG/src/items/treasure.py
@@ -0,0 +1,11 @@
+'''
+Created on 05/apr/2015
+
+@author: marcello
+'''
+
+class Treasure(object):
+    '''Base abstract class for money and valuables'''
+    
+    pass
+        
---
+++ PyRPG/src/items/weapons.py
@@ -0,0 +1,10 @@
+'''
+Created on 05/apr/2015
+
+@author: marcello
+'''
+
+class Weapon(object):
+    '''Abstract base class for weapons'''
+    
+    pass

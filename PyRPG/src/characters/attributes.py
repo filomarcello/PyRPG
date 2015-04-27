cbd48930183683107b02987af63ea9c1bf952929
filomarcello <marcello.filopanti@gmail.com>
2015-04-27 22:28:25 +0200

    Set and tested Abilities class

--- PyRPG/src/characters/attributes.py
+++ PyRPG/src/characters/attributes.py
@@ -3,8 +3,9 @@ Created on 20/apr/2015
 
 @author: marcello
 '''
-from _ast import Str
 
+DD_ABILITIES_NAMES = ['strength', 'dexterity', 'constitution', 'intelligence',
+                      'wisdom', 'charisma']
 
 class Abilities(object):
     '''Base class for ability scores.
@@ -14,18 +15,63 @@ class Abilities(object):
     '''
 
     def __init__(self, abilities: dict = {'attack': 1}):
-        self.abilities = abilities
+        '''Creates an object with a dict of abilities.
+        
+        If abilities is not specified uses a standard one:
+        >>> ab = Abilities()
+        >>> ab.get_ability('attack')
+        1
+        
+        No other abilities has been created:
+        >>> ab.get_ability('dementia')
+        Traceback (most recent call last):
+            ...
+        KeyError: 'dementia'
+        '''
+        self._abilities = abilities
         
     def set_ability(self, ability_name: str, value):
         '''Set the ability_name to the value (numeric).
         
-        If 'ability_name' ability does not exist, the function creates a
-        new one.
+        >>> ab.set_ability('attack', 10)
+        >>> ab.get_ability('attack')
+        10
         '''
-        self.abilities[ability_name] = value
+        self._abilities[ability_name] = value
         
-    def get_ability(self, ability_name: str):
+    def get_ability(self, ability_name: str) -> 'Number':
         '''Get the value (numeric) of 'ability_name' ability.'''
-        return self.abilities[ability_name]
+        return self._abilities[ability_name]
     
+
+class DD_Abilities(Abilities): #TODO: sistemare 
+    '''Set then abilities to the standard D&D.
     
+    Preset abilities are strength, dexterity, constitution, intelligence,
+    wisdom, and charisma. 
+    '''
+    
+    def __init__(self, values: list = [0, 0, 0, 0, 0, 0]):
+        '''Order of the values list: str, dex, con, int, wis, cha.
+        
+        Default are zero values.
+        >>> dd_ab = DD_Abilities()
+        >>> dd_ab.get_ability('constitution')
+        0
+        
+        This constructor does not check if values are in range for D&D rules:    
+        >>> dd_ab2 = DD_Abilities([1000, 3000, -69, 10.25, 858, 0)
+        >>> dd_ab2.get_ability('constitution')
+        -69
+        >>> dd_ab2.get_ability('wisdom')
+        '''
+        super().__init__(abilities={n: v for n, v 
+                                    in zip(DD_ABILITIES_NAMES, values)})
+                                    
+                                   
+
+
+# testing by doctest         
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod() 

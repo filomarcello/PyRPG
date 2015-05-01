173d9fddb8a65502d8e2f76e9b6122bb6c29e134
filomarcello <marcello.filopanti@gmail.com>
2015-05-01 22:41:58 +0200

    Test and completing item and wearables

--- PyRPG/src/items/item.py
+++ PyRPG/src/items/item.py
@@ -7,4 +7,30 @@ Created on 05/apr/2015
 class Item(object):
     '''Base abstract class for a generic item.'''
     
-    pass
+    def __init__(self, name: str = 'noname', weight: float = 0.0):
+        '''Absolutely generic item.
+        
+        >>> i = Item()
+        >>> print(i)
+        Item: noname
+        Weight: 0.0
+        '''
+        self._name = name
+        self._weight = weight
+        
+    def get_weight(self):
+        '''Returns the weight of the Item.
+        
+        >>> i = Item(weight=12.5)
+        >>> i.get_weight()
+        12.5
+        '''
+        return self._weight
+        
+    def __str__(self):
+        return 'Item: {s._name}\nWeight: {s._weight}'.format(s=self)
+    
+# testing by doctest         
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod() 
---
+++ PyRPG/src/items/wearables.py
@@ -0,0 +1,45 @@
+'''
+Created on 05/apr/2015
+
+@author: marcello
+'''
+from items.item import Item
+
+class Wearable(Item):
+    '''Base abstract class for a generic wearable item.
+    
+    May include armors, helmet, robe, gloves, boots, etc.
+    '''
+    
+    def __init__(self, name: str, weight: float = 0.0 , worn: bool = False):
+        '''Build a Wearable with name, weight and boolean worn.
+        
+        >>> w = Wearable(name='boots')
+        >>> print(w)
+        Wearable: boots
+        Weight: 0.0
+        Worn: False
+        >>> w.get_weight()
+        0.0
+        '''
+        super().__init__(name, weight)
+        self._worn = worn
+        
+    def is_worn(self):
+        '''Return True if the Wearable is worn
+        
+        >>> w = Wearable(name='gloves', weight=0.5, worn=True)
+        >>> w.is_worn()
+        True
+        '''
+        return self._worn
+    
+    def __str__(self):
+        return 'Wearable: {s._name}\nWeight: {s._weight}\nWorn: {s._worn}'\
+            .format(s=self)
+    
+    
+# testing by doctest         
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod() 
